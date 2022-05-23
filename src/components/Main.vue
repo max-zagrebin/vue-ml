3b293c49fef29dce0820b7b603ab8bda971227f5
zagrebin.mc <zagrebin.mc@gmail.com>
2022-05-23 15:54:14 +0400

    main.vue, pic

---
+++ src/components/Main.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="page">
+<!--    <img class="back-image" src="assets/example.png" alt="example-pic">-->
+    <canvas></canvas>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.back-image{
+  max-height: 500px;
+
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
