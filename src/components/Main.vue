19e77d55079eacfb11a21111c84963753bd97c16
max-zagrebin <76188882+max-zagrebin@users.noreply.github.com>
2023-04-13 04:32:51 +0400

    Update Main.vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="page">
+
     <div class="background">
       <div class="canvas-header">
         <div class="header-text">
-          Зал Рафаэля
+          Зал Рафаэля {{ debugX }} {{ debugY }}
         </div>
       </div>
       <canvas
@@ -25,6 +26,7 @@
         <p>ЛКМ - добавление точки</p>
         <p>ПКМ - удаление точки</p>
         <p>Зажатие ЛКМ - перемещение существующей точки</p>
+        <p>{{ debugX }} {{ debugY }}</p>
       </div>
       <ul class="pointList">
         <li v-for="(point, index) in pointsArray" :key="`point-${index}`">{{ point.x }}, {{ point.y }}</li>
@@ -46,7 +48,9 @@ export default {
       dragMode: false,
       selectedElem: null,
       constructActionName: "Построить область",
-      img: null
+      img: null,
+      debugX: 0,
+      debugY: 0
     };
   },
   mounted() {
@@ -73,9 +77,25 @@ export default {
     outerPathSelPoint: function (path, x, y) {
       path.arc(x, y, 11, 0, Math.PI * 2, true);
     },
-    drawPointsArray: function (ctx, regionMode, selElem) {
+    drawPointsArray: function (ctx, regionMode, selElem, linePoint) {
       if (this.pointsArray.length === 0) return;
       this.clearCanvas(ctx);
+
+      if (linePoint) {
+        // console.log("LINE POINT PRESENT");
+        let innerPointsLinePath = new Path2D();
+        let outerPointsLinePath = new Path2D();
+        innerPointsLinePath.moveTo(linePoint.x, linePoint.y);
+        this.innerPathPoint(innerPointsLinePath, linePoint.x, linePoint.y);
+        outerPointsLinePath.moveTo(linePoint.x, linePoint.y);
+        this.outerPathPoint(outerPointsLinePath, linePoint.x, linePoint.y);
+        ctx.fillStyle = "#2F80ED";
+        ctx.fill(outerPointsLinePath);
+        ctx.fillStyle = "#FFFFFF";
+        ctx.fill(innerPointsLinePath);
+        ctx.fillStyle = "#FFFFFF";
+      }
+
       let innerPointsPath = new Path2D();
       let outerPointsPath = new Path2D();
       let innerPointsSelPath = new Path2D();
@@ -85,7 +105,6 @@ export default {
       innerPointsPath.moveTo(this.pointsArray[0].x, this.pointsArray[0].y);
       outerPointsPath.moveTo(this.pointsArray[0].x, this.pointsArray[0].y);
       this.pointsArray.forEach((elem) => {
-        regionPath.lineTo(elem.x, elem.y);
         if (elem === selElem) {
           innerPointsSelPath.moveTo(elem.x, elem.y);
           this.innerPathSelPoint(innerPointsSelPath, elem.x, elem.y);
@@ -102,6 +121,15 @@ export default {
         outerPointsPath.moveTo(elem.x, elem.y);
         this.outerPathPoint(outerPointsPath, elem.x, elem.y);
       });
+
+
+      for (let elem of this.pointsArray) {
+        regionPath.lineTo(elem.x, elem.y);
+      }
+      // this.pointsArray.forEach((elem) => {
+      //
+      //
+      // });
       if (regionMode) {
         ctx.fillStyle = "rgba(47,128,237,0.22)";
         ctx.fill(regionPath);
@@ -137,33 +165,199 @@ export default {
       ctx.rect(0, 0, this.canvasSize.width, this.canvasSize.height);
       this.vueCanvas.drawImage(this.img, 0, 0);
     },
-    mouseUp: function (event) {
+    mouseUp: function () {
+
+
+      this.dragMode = false;
+    },
+    mouseDown: function (event) {
       console.log(event);
+      let rect = event.target.getBoundingClientRect();
+      let x = Math.abs(event.clientX - rect.left);
+      let y = Math.abs(event.clientY - rect.top);
+
+      let mouseElem = {x, y};
+      console.log("MOUSE DOWN");
+      console.log(mouseElem);
+
+      let targetIndex = this.pointsArray.length;
+      // let minHypo = Number.MAX_VALUE;
+      // let minElem = this.pointsArray[0];
+      // for (let elem of this.pointsArray) {
+      //   let diffX = mouseElem.x - elem.x;
+      //   let diffY = mouseElem.y - elem.y;
+      //   // let hypo = Math.sqrt(Math.pow(diffX,2) + Math.pow(diffY,2));
+      //   let hypo = Math.hypot(diffX, diffY);
+      //   console.log("CHECK ELEM");
+      //   console.log(elem);
+      //   console.log("CURRENT HYPO");
+      //   console.log(hypo);
+      //   if (hypo < minHypo) {
+      //
+      //     minHypo = hypo;
+      //     minElem = elem;
+      //     console.log("NEW MIN");
+      //     console.log(hypo);
+      //   }
+      //   // console.log("DISTANCE");
+      //   // console.log(elem);
+      //
+      //   // if (x <= elem.x + radius && x >= elem.x - radius && y <= elem.y + radius && y >= elem.y - radius) {
+      //   //   this.selectedElem = elem;
+      //   //   this.canvasElem.style.cursor = "pointer";
+      //   //   return this.drawPointsArray(this.vueCanvas, this.regionMode, elem);
+      //   // }
+      // }
+      // console.log("TOTAL CLOSEST //////////");
+      // console.log(minElem);
+      // console.log(minHypo);
+      // let targetIndex = this.pointsArray.length;
+      // if (this.pointsArray.length > 2) {
+      //   targetIndex = this.pointsArray.indexOf(minElem);
+      //   // if (this.pointsArray.length >= targetIndex + 1) {
+      //   // if (this.pointsArray[targetIndex] === this.pointsArray.length - 1) {
+      //   //   targetIndex = 0;
+      //   // }
+      //   console.log("PREV ELEM");
+      //   console.log(this.pointsArray[targetIndex - 1] ?? this.pointsArray[this.pointsArray.length - 1]);
+      //   console.log("NEXT ELEM");
+      //   console.log(this.pointsArray[targetIndex + 1] ?? this.pointsArray[0]);
+      //   let prevElem = this.pointsArray[targetIndex - 1];
+      //   let nextElem = this.pointsArray[targetIndex + 1];
+      //
+      //   // let prevHypodiffX = mouseElem.x - elem.x;
+      //   // let diffY = mouseElem.y - elem.y;
+      //   // // let hypo = Math.sqrt(Math.pow(diffX,2) + Math.pow(diffY,2));
+      //   // let hypo = Math.hypot(diffX, diffY);
+      //
+      //
+      //   let prevHypo = Math.hypot(mouseElem.x - prevElem.x, mouseElem.y - prevElem.y);
+      //   let nextHypo = Math.hypot(mouseElem.x - nextElem.x, mouseElem.y - nextElem.y);
+      //   console.log("PREV HYPO");
+      //   console.log(prevHypo);
+      //   console.log("NEXT HYPO");
+      //   console.log(nextHypo);
+      //   if (prevHypo > nextHypo) {
+      //     targetIndex += 1;
+      //   }
+      // }
+
+
       if (!this.selectedElem) {
         let rect = event.target.getBoundingClientRect();
         let x = event.clientX - rect.left;
         let y = event.clientY - rect.top;
         let newElem = {x, y};
-        this.pointsArray.push(newElem);
+        console.log("ADD AT TARGET INDEX " + targetIndex);
+        this.pointsArray.splice(targetIndex, 0, newElem);
+        // this.pointsArray.push(newElem);
         this.canvasElem.style.cursor = "pointer";
         this.drawPointsArray(this.vueCanvas, this.regionMode, this.pointsArray[this.pointsArray.length - 1]);
       }
-      this.dragMode = false;
-    },
-    mouseDown: function (event) {
-      console.log(event);
       this.dragMode = true;
     },
-    mouseLeave: function (event) {
-      console.log(event);
+    mouseLeave: function () {
+      // console.log(event);
       this.dragMode = false;
     },
+
+
     mouseMove: function (event) {
       let rect = event.target.getBoundingClientRect();
       let x = event.clientX - rect.left;
       let y = event.clientY - rect.top;
+      this.debugX = x;
+      this.debugY = y;
       // console.log("Coordinate x: " + x, "Coordinate y: " + y);
       let radius = 7;
+
+      let linePoint;
+
+      // console.log("POINTS ARRAY");
+      let curPoint;
+      let prevPoint;
+      linePoint = {x, y};
+      // if (this.pointsArray.length > 0) {
+      //   let point = this.pointsArray[0];
+      //
+      //   // console.log("MOUSE");
+      //   // console.log(linePoint);
+      //   // console.log("POINT");
+      //   // console.log(point);
+      //   console.log("MOUSE ANGLE");
+      //   console.log(mouseAngle);
+      // }
+      let anglesMeet = false;
+      for (let i = 1; i < this.pointsArray.length; i++) {
+        // console.log(this.pointsArray[i]);
+        curPoint = this.pointsArray[i];
+        prevPoint = this.pointsArray[i - 1];
+        // let angleDeg = (Math.atan2(curPoint.y - prevPoint.y, curPoint.x - prevPoint.x) * 180 / Math.PI).toFixed(0);
+        // let mouseAngle = (Math.atan2(curPoint.y - linePoint.y, curPoint.x - linePoint.x) * 180 / Math.PI).toFixed(0);
+        console.log("CUR POINT");
+        console.log(curPoint.x);
+        console.log(curPoint.y);
+        console.log("MOUSE POINT");
+        console.log(linePoint.x);
+        console.log(linePoint.y);
+
+        // let angleDeg = Math.tan((curPoint.y - prevPoint.y) / (curPoint.x - prevPoint.x));
+        // let mouseAngle = Math.tan((curPoint.y - linePoint.y )/ (curPoint.x - linePoint.x));
+        let angleDeg = Math.atan2(curPoint.y - prevPoint.y, curPoint.x - prevPoint.x);
+        let mouseAngle = Math.atan2(curPoint.y - linePoint.y, curPoint.x - linePoint.x);
+        console.log("DOTS ANGLE ////");
+        console.log(angleDeg);
+        console.log("MOUSE ANGLE1");
+        console.log(mouseAngle);
+        console.log("DIFF");
+        console.log(Math.abs(angleDeg - mouseAngle));
+
+
+        if (Math.abs(angleDeg - mouseAngle) < 0.02) {
+          anglesMeet = true;
+          console.log(Math.tan(angleDeg));  //(curPoint.y - linePoint.y) / (curPoint.x - linePoint.x)
+          // console.log(Math.tan(angleDeg)*(curPoint.x - linePoint.x));
+          console.log(Math.tan(angleDeg) / (curPoint.x - linePoint.x) + linePoint.x);
+          console.log(Math.tan(angleDeg) / (curPoint.y - linePoint.y) + linePoint.y);
+          // console.log(Math.tan(angleDeg)/(curPoint.x - prevPoint.x) + prevPoint.x);
+          // console.log(Math.tan(angleDeg)/(curPoint.y - prevPoint.y) + prevPoint.y);
+          // console.log(Math.tan(angleDeg)*(curPoint.x - prevPoint.x));
+          // console.log(((curPoint.x - linePoint.x )/Math.tan(angleDeg) - prevPoint.x));
+          // console.log(Math.tan(angleDeg)*(curPoint.y - linePoint.y));
+          // console.log(Math.tan(mouseAngle));
+          // console.log(Math.tan(angleDeg));
+          // console.log(Math.tan(mouseAngle));
+          // console.log(curPoint.yangleDeg / curPoint.y);
+          // console.log(angleDeg / curPoint.x);
+          // linePoint = {x, y};
+          let saveX = linePoint.x;
+          let saveY = linePoint.y;
+          console.log("TG");
+          console.log(Math.tan(angleDeg));
+          console.log((curPoint.y - prevPoint.y) / (curPoint.x - prevPoint.x));
+          console.log(curPoint.x - prevPoint.x);
+          console.log(curPoint.y - prevPoint.y);
+          console.log(curPoint.x - linePoint.x);
+          console.log(curPoint.y - linePoint.y);
+          // linePoint.x = Math.tan(angleDeg) / (curPoint.x - prevPoint.x) + Math.round(saveX);
+          // linePoint.y = Math.tan(angleDeg) / (curPoint.y - prevPoint.y) + Math.round(saveY);
+          linePoint.x = saveX - (curPoint.x - prevPoint.x);
+
+          linePoint.y = (curPoint.y - prevPoint.y) * linePoint.x / (curPoint.x - prevPoint.x);
+          // linePoint.y = saveY;
+          console.log(linePoint);
+          // linePoint = {x :, y : };
+          break;
+        }
+      }
+      if (!anglesMeet) {
+        linePoint = undefined;
+      }
+      // for (let elem of this.pointsArray) {
+      //   if()
+      // }
+
+
       if (this.selectedElem && this.dragMode) {
         this.selectedElem.x = x;
         this.selectedElem.y = y;
@@ -172,13 +366,14 @@ export default {
         if (x <= elem.x + radius && x >= elem.x - radius && y <= elem.y + radius && y >= elem.y - radius) {
           this.selectedElem = elem;
           this.canvasElem.style.cursor = "pointer";
-          return this.drawPointsArray(this.vueCanvas, this.regionMode, elem);
+
+          return this.drawPointsArray(this.vueCanvas, this.regionMode, elem, linePoint);
         }
       }
       this.canvasElem.style.cursor = "default";
       this.selectedElem = null;
       this.dragMode = false;
-      return this.drawPointsArray(this.vueCanvas, this.regionMode);
+      return this.drawPointsArray(this.vueCanvas, this.regionMode, undefined, linePoint);
     },
     mouseRightButton: function (event) {
       let rect = event.target.getBoundingClientRect();
